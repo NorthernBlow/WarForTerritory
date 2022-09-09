72a14fe917c8d66b3dff7dfdd30ab161688ac046
NorthernBlow <rheinhart@yandex.ru>
2022-09-09 14:51:17 +0300

    so it begins in two

--- main.lua
+++ main.lua
@@ -1,5 +1,12 @@
-    function love.draw()
-    love.graphics.print("Hello World!", 1024, 768)
-    end
+function love.draw()
+  love.graphics.print("Hello World!", 1024, 768)
+  end
   
-  
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
