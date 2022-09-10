b7afae9bf5f4723c5edf357eeaae61a4d83c671f
NorthernBlow <rheinhart@yandex.ru>
2022-09-10 11:55:20 +0300

    resize fullscreen mode

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 function love.draw()
-  love.graphics.print("Hello World!", 1024, 768)
-  end
+  love.graphics.print("Hello World!", 19, 19)
+  love.window.setTitle("SUPER GAME")
+end
   
 function love.load()
 
@@ -10,3 +11,21 @@ function love.update(dt)
 
 end
 
+function love.keypressed(key, scancode, isrepeat)
+	if key == "f11" then
+		fullscreen = not fullscreen
+		love.window.setFullscreen(fullscreen, "desktop")
+	end
+end
+
+function train(num)
+  num = 2
+  if num > 0 then
+    print(num)
+    end
+
+end
+
+
+a1 = train()
+print(a1)
