1710355eecd7a3174b921d05cfd69295885c24de
NorthernBlow <rheinhart@yandex.ru>
2022-09-21 01:37:54 +0300

    set exghaust

--- main.lua
+++ main.lua
@@ -1,19 +1,32 @@
 function love.load()
+  
+  --таблцица 
+  pz3mtank = {}
+  pz3mtank[1] = love.graphics.newImage("/pz3m.png")
+  pz3mtank[2] = love.graphics.newImage("/pz3mEXGHAUST.png")
   love.window.setTitle("War For Territory")
-  pz3m = love.graphics.newImage("/pz3m.png")
+  
+  --координаты
   x, y = 100, 100
+  
+  --скорость
   speed = 100
   speed2 = 10
   right = false
+  frame = 1
+  slow = 0
+  isDamaged = 0.1
 end
 
 function love.draw()
   
-love.graphics.clear(0, 20, 130)  
+love.graphics.clear(0, 20, 130)
+
+--прописываем угол поворта
 if right then
-  love.graphics.draw( pz3m, x, y, math.pi / 2) 
+  love.graphics.draw(pz3mtank[frame], x, y, math.pi / 2, nil, nil, 30, 36) 
   else
-  love.graphics.draw(pz3m, x, y, 0)
+  love.graphics.draw(pz3mtank[frame], x, y, 0, nil, nil, 30, 36 )
   end
 end
 
@@ -22,6 +35,13 @@ function love.update(dt)
   x = x + speed * dt
   y = y + speed2 * dt
   right = love.mouse.isDown(2)
+  frame = frame + 1
+  if slow > isDamaged then
+    slow = 0
+    if frame > 2 then
+    frame = 1
+  end
+  end
 end
 
 function love.keypressed(key, scancode, isrepeat)
